de4e9d299c63129cb5ab83b2f208b52e4d21143d
terikaisterika <trililium@yandex.ru>
2024-07-08 14:49:00 +0300

    добавлен скрипт для работы с websocket

--- get-information/app.js
+++ get-information/app.js
@@ -10,6 +10,9 @@ const hostRest = '127.0.0.1';
 const portRest = 3000;
 
 let clients = [];
+let clientsWorkSpace = [];
+let nicknames = {};
+//Получить количество сущностей из rest api
 function getCount(entity, socket) {
   let url = `http://admin:123@127.0.0.1:3000/${entity}`;
   request(url, async (err, res, body) => {
@@ -27,6 +30,7 @@ function getCount(entity, socket) {
     }
   });
 }
+
 io.on('connection', (socket) => {
   console.log(`Client with id ${socket.id} connected`);
   clients.push(socket.id);
@@ -38,17 +42,30 @@ io.on('connection', (socket) => {
 
   socket.on('message', (message) => {
     let answer = '';
-
     switch (message) {
       case 'yes':
-        answer =
-          'Если хотите узнать количество существующих юзеров, введите users. Количество задач можно узнать введя tasks';
+        answer = `Если хотите узнать количество существующих юзеров, введите users.
+          Количество задач можно узнать введя tasks.
+          Чтобы оповестить всех о своем подключении, отправьте all.
+          Если надо подключиться к рабочему пространству, то это можно сделать по урлу ws://${host}:${port}/work.`;
         break;
       case 'users':
         getCount(message, socket);
         break;
       case 'tasks':
         getCount(message, socket);
+        break;
+      case 'all':
+        io.sockets.emit(
+          'informEveryone',
+          `К нам присоединился пользователь с ${socket.id}`
+        );
+        break;
+      case 'broadcast':
+        socket.broadcast.emit(
+          'message',
+          `Это пример, когда сообщение получают все, кроме отправителя`
+        );
         break;
       default:
         answer = `Я не знаю, что Вам ответить. Меня к такому не готовили `;
@@ -63,7 +80,88 @@ io.on('connection', (socket) => {
     console.log(`Client with id ${socket.id} disconnected`);
   });
 });
-
+// Работа с пространствами и комнатами
+const work_space = io.of('/work');
+work_space.on('connection', (socket) => {
+  console.log(`Client with id ${socket.id} connected`);
+  let answer = '';
+  const introdactionRoom = 'introdactionRoom';
+  let rooms = work_space.adapter.rooms;
+  clientsWorkSpace.push(socket.id);
+  work_space
+    .to(socket.id)
+    .emit(
+      'message',
+      'Соединение установлено. Вы присоединились к пространству work. Тут обсуждаются рабочие вопросы'
+    );
+  socket.on('message', (message) => {
+    switch (message) {
+      case 'help':
+        socket.emit(
+          'message',
+          `Для только присоединившихся к нашей команде отправьте слово introduction.\n 
+          Так Вы присоединитесь к обучающимся. Остальные могут просто отправлять сообщения.`
+        );
+        break;
+      case 'introduction':
+        socket.join(introdactionRoom);
+        work_space.to(socket.id).emit(
+          'message',
+          `Тут Вы получите помощь для новичков.
+           Общие инструкции можно получить, отправив слово instructions`
+        );
+        break;
+      case 'instructions':
+        if (socket.rooms.has(introdactionRoom)) {
+          answer = `В первый день нужно ознакомиться с должностными инструкциями на главной странице.
+          Далее установить необходимые инструменты.
+          Запрашивать доступы нет необходимости.
+          После подтверждения Вашей личности запросы на доступы были разосланы автоматически.
+          Список приложений и доступов к ним можно будет проверить на вашей личной странице после 12:00.
+          Если Вам не хватает какой-то информации, задайте вопрос.
+          Мы разошлем его всем в комнате обущающихся.
+          Выйти из из комнаты instructions можно отправив слово complete.
+          Если нужно отправить сообщение за пределы комнаты, отправьте сообщение с типом события question. 
+          Его получат все, кто прослушивает это событие и может ответить на вопросы новичков.`;
+        } else {
+          answer = `Чтобы воспользоваться командой, присоединитесь к комнате обучающихся через команду introduction`;
+        }
+        work_space.to(socket.id).emit('message', answer);
+        break;
+      case 'complete':
+        if (socket.rooms.has(introdactionRoom)) {
+          socket.leave(introdactionRoom);
+          answer = `Вы покинули комнату ${introdactionRoom}`;
+        } else {
+          answer = `Вы не были в комнате ${introdactionRoom}`;
+        }
+        work_space.to(socket.id).emit('message', answer);
+        break;
+      case 'count':
+        answer = rooms.has(introdactionRoom)
+          ? rooms.get(introdactionRoom).size
+          : 0;
+        work_space.to(socket.id).emit('message', answer);
+        break;
+      default:
+        socketInRoom = socket.rooms.has(introdactionRoom);
+        if (message && answer == '' && socketInRoom) {
+          socket.broadcast
+            .in(introdactionRoom)
+            .emit('message', `${message} (msg для комнаты)`);
+        } else if (message && socketInRoom == false) {
+          socket.broadcast.emit('message', `${message} (msg для всех)`);
+        }
+    }
+  });
+  socket.on('question', (message) => {
+    if (message) work_space.broadcast.emit('question', message);
+  });
+  socket.on('disconnect', () => {
+    clientsWorkSpace.splice(clientsWorkSpace.indexOf(socket.id), 1);
+    console.log(`Client with id ${socket.id} disconnected`);
+  });
+});
 app.use(express.static(__dirname));
 http.listen(port, host, () =>
   console.log(`Server listens http://${host}:${port}`)
---
+++ get-information/pure_wss.js
@@ -0,0 +1,87 @@
+const WebSocketServer = require('ws');
+const request = require('request');
+let clients = {};
+const port = 7001;
+let webSocketServer = new WebSocketServer.Server({
+  port: port,
+});
+/**
+ * Отправка сообщения всем, кроме себя
+ * @param {*} clients объект подключенных клиентов
+ * @param {*} message сообщение, которое планируем отправить
+ * @param {*} id свое id, которое исключает это id из получателей
+ */
+function sendMessageAll(clients, message, id) {
+  for (const key in clients) {
+    if (key == id) continue;
+    clients[key].send(message);
+  }
+}
+//Отправка сообщения от сервера только себе
+function sendMessageYourself(mySocket, message) {
+  mySocket.send(message);
+}
+/**
+ * Получение количества юзеров или задач от rest api
+ * возможные значения entity 'users' и 'tasks'
+ * @param {*} entity
+ * @param {*} mySocket
+ */
+function getCount(entity, mySocket) {
+  let url = `http://admin:123@127.0.0.1:3000/${entity}`;
+  request(url, async (err, res, body) => {
+    if (err) {
+      console.log(`У нас случилась ошибка, мы уже чиним. 
+        Текст ошибки: ${err}.
+        Хотя скорее всего Вы просто не запустили проект с Rest API`);
+      sendMessageYourself(
+        mySocket,
+        `У нас случилась ошибка, мы уже чиним.
+        Текст ошибки: ${err}.
+        Хотя скорее всего Вы просто не запустили проект с Rest API`
+      );
+      return;
+    } else {
+      const resBody = res.body;
+      const answer = JSON.parse(resBody).length;
+      sendMessageYourself(mySocket, answer);
+      return;
+    }
+  });
+}
+// Основа взаимодействия сервера с клиентом.
+webSocketServer.on('connection', (ws) => {
+  let answer = '';
+  const id = Math.random();
+  clients[id] = ws;
+  clients[id].send(
+    `Соединение установлено. Для получения инструкции отправьте слово help`
+  );
+  console.log('Установлено соединение ' + id);
+  ws.on('message', (message) => {
+    message = message.toString();
+    switch (message) {
+      case 'help':
+        answer = `Если хотите узнать количество существующих юзеров в REST API, введите users.
+          Количество созданных задач в REST API можно узнать введя tasks.
+          Для общения с остальными подключенными юзерами просто отправляйте сообщения`;
+        sendMessageYourself(clients[id], answer);
+        break;
+      case 'users':
+        getCount(message, clients[id]);
+        break;
+      case 'tasks':
+        getCount(message, clients[id]);
+        break;
+      default:
+        if (message) {
+          sendMessageAll(clients, message, id);
+        }
+    }
+  });
+  ws.on('close', () => {
+    console.log(console.log('соединение закрыто ', id));
+    delete clients[id];
+  });
+});
+console.log(`Сервер запущен на ws://127.0.0.1:${port}`);
